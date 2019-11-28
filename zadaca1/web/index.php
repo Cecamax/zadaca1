27c957d42383645f973d7ba9cea0120b1ea79b42
Cecamax <57287400+Cecamax@users.noreply.github.com>
2019-11-28 15:03:40 +0100

    Add files via upload

---
+++ zadaca1/web/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- Head: start-->
+    <?php require 'pages/head.html.php'; ?>
+    <!-- Head: end-->
+
+<body data-spy="scroll" data-target=".fixed-top">
+    
+    <!-- Preloader:start --> 
+    <?php require 'pages/preloader.html.php'; ?>
+    <!-- Preloader:end-->
+    
+
+    <!-- Navigation: start -->
+    <?php include 'pages/navigation.html.php'; ?>
+    <!-- Nvigation:end -->
+
+
+    <!-- Header: start -->
+    <?php require 'pages/header.html.php'; ?>
+    <!-- Header: end -->
+
+
+    <!-- Testimonials: start -->
+    <?php require 'pages/testimonials.html.php'; ?>
+    <!-- Testimonials: end -->
+    
+
+    <!-- section3: start -->
+    <!-- Features -->
+    <?php require 'pages/section3.html.php'; ?>
+    <!-- end of features -->
+    <!-- section3: end -->
+
+
+    <!-- Video: start -->
+    <?php require 'pages/video.html.php'; ?>
+    <!-- Video: end -->
+
+
+    <!-- Details 1: start -->
+    <?php require 'pages/details_1.html.php'; ?>
+    <!-- Details 1: end -->
+
+
+    <!-- Details 2: start -->
+    <?php require 'pages/details_2.html.php'; ?>
+    <!-- Details 2: end -->
+
+
+    <!-- Details Lightboxes -->
+	<?php require 'pages/lightboxes.html.php'; ?>
+    <!-- end of details lightboxes -->
+
+
+    <!-- Screenshots: strt -->
+    <?php require 'pages/screenshots.html.php'; ?>
+    <!-- Screenshots: end -->
+
+
+    <!-- Download: start -->
+    <?php require 'pages/download.html.php'; ?>
+    <!-- Download: end -->
+
+
+    <!-- Statistics: start -->
+    <?php require 'pages/statistics.html.php'; ?>
+    <!-- Statistics: end -->
+
+
+    <!-- Contact: start -->
+    <?php require 'pages/contacts.html.php'; ?>
+    <!-- Contact: end -->
+
+
+    <!-- Footer: star -->
+    <?php include 'pages/footer.html.php'; ?> 
+    <!-- Footer: end -->
+
+
+    <!-- Copyright: start -->
+    <?php require 'pages/copyright.html.php'; ?>
+    <!-- Copyright: end -->
+    
+    	
+    <!-- Scripts -->
+    <script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+    <script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+    <script src="js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="js/morphext.min.js"></script> <!-- Morphtext rotating text in the header -->
+    <script src="js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="js/scripts.js"></script> <!-- Custom scripts -->
+</body>
+</html>
